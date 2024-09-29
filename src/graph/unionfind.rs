3df4406b7b413f135536a2f9c8e7e543c649e9b8
kamomearm <aymsuq@gmail.com>
2024-09-29 19:08:43 +0900

    ufを変更

--- src/graph/unionfind.rs
+++ src/graph/unionfind.rs
@@ -1,20 +1,75 @@
 use cargo_snippet::snippet;
 
-#[snippet]
+pub struct PotentialedUnionFind {
+    len: usize,
+    par: Vec<usize>,
+    size: Vec<usize>,
+    diff_weight: Vec<i64>,
+}
+impl PotentialedUnionFind {
+    pub fn new(n: usize) -> Self {
+        PotentialedUnionFind {
+            len: n,
+            par: (0..n).collect(),
+            size: vec![0; n],
+            diff_weight: vec![0; n],
+        }
+    }
+    pub fn root(&mut self, x: usize) -> usize {
+        if self.par[x] == x {
+            x
+        } else {
+            let r = self.root(self.par[x]);
+            self.diff_weight[x] += self.diff_weight[self.par[x]];
+            self.par[x] = r;
+            r
+        }
+    }
+    pub fn weight(&mut self, x: usize) -> i64 {
+        self.root(x);
+        self.diff_weight[x]
+    }
+    pub fn diff(&mut self, x: usize, y: usize) -> i64 {
+        //! `x`と`y`の差
+        self.weight(x) - self.weight(y)
+    }
+    pub fn unite(&mut self, x: usize, y: usize, w: i64) -> bool {
+        //! `weight(y) - weight(x) == w`となるようにunite
+        let mut w = w;
+        w += self.weight(x);
+        w -= self.weight(y);
+
+        let mut x = self.root(x);
+        let mut y = self.root(y);
+        if x == y {
+            return false;
+        }
+        if self.size[x] < self.size[y] {
+            std::mem::swap(&mut x, &mut y);
+            w = -w;
+        }
+        self.par[y] = x;
+        self.size[x] += self.size[y];
+        self.diff_weight[y] = w;
+        true
+    }
+}
+
+#[snippet("UnionFind")]
 pub struct UnionFind {
-    v_size: usize,
+    len: usize,
     par: Vec<i64>,
-    siz: Vec<usize>,
-    edgecount: Vec<usize>,
+    size: Vec<usize>,
+    // edgecount: Vec<usize>,
 }
 #[snippet("UnionFind")]
 impl UnionFind {
     pub fn new(n: usize) -> Self {
         UnionFind {
-            v_size: n,
+            len: n,
             par: vec![-1; n],
-            siz: vec![1; n],
-            edgecount: vec![0; n],
+            size: vec![1; n],
+            // edgecount: vec![0; n],
         }
     }
 
@@ -26,26 +81,21 @@ impl UnionFind {
             self.par[x] as usize
         }
     }
-    
+
     pub fn unite(&mut self, x: usize, y: usize) -> bool {
-        let rx = self.root(x);
-        let ry = self.root(y);
-        if rx == ry {
-            self.edgecount[rx] += 1;
+        let mut x = self.root(x);
+        let mut y = self.root(y);
+        if x == y {
             return false;
         }
-        if self.siz[rx] < self.siz[ry] {
-            self.par[rx] = ry as i64;
-            self.siz[ry] += self.siz[rx];
-            self.edgecount[ry] += 1 + self.edgecount[rx];
-        } else {
-            self.par[ry] = rx as i64;
-            self.siz[rx] += self.siz[ry];
-            self.edgecount[rx] += 1 + self.edgecount[ry];
+        if self.size[x] < self.size[y] {
+            std::mem::swap(&mut x, &mut y);
         }
+        self.par[y] = x as i64;
+        self.size[x] += self.size[y];
         true
     }
-    
+
     pub fn issame(&mut self, x: usize, y: usize) -> bool {
         if self.root(x) == self.root(y) {
             return true;
@@ -55,29 +105,25 @@ impl UnionFind {
 
     pub fn size(&mut self, x: usize) -> usize {
         let r = self.root(x);
-        self.siz[r]
+        self.size[r]
     }
 
-    pub fn edge(&mut self, x: usize) -> usize {
-        let r = self.root(x);
-        self.edgecount[r]
-    }
-    
-    pub fn groupcout(&mut self) -> usize {
-        let mut dic: std::collections::HashMap<usize, usize> = std::collections::HashMap::new();
-        for i in 0..self.v_size {
-            let r = self.root(i);
-            *dic.entry(r).or_default() += 1;
+    // pub fn edge(&mut self, x: usize) -> usize {
+    //     let r = self.root(x);
+    //     self.edgecount[r]
+    // }
+
+    pub fn group_count(&mut self) -> usize {
+        //!  `UnionFind`内の集合の数を返す
+        //!
+        //! `O(Nα(N)) `
+        let mut cnt = vec![0; self.len];
+        for i in 0..self.len {
+            cnt[self.root(i)] += 1;
         }
-        let mut ret = 0;
-        for value in dic.values() {
-            ret = ret.max(*value);
-        }
-        ret
+        cnt.iter().filter(|&&i| 0 < i).count()
     }
 }
 
 #[cfg(test)]
-mod test {
-
-}
+mod test {}
