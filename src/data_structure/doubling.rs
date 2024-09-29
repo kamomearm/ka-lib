e3e2f83c6ee18579b0e7ef5eaf0962bc8f90024c
kamomearm <aymsuq@gmail.com>
2024-09-29 13:05:05 +0900

    ダブリングトレイトをtrait.rsに移動

--- src/data_structure/doubling.rs
+++ src/data_structure/doubling.rs
@@ -1,12 +1,6 @@
-use crate::traits::Monoid;
+use crate::traits::{Doubling, Monoid};
 
-/// 位置`p`から`k`ターン先の行き先を求める
-pub trait Doubling {
-    type D;
-    fn new(v: &[Self::D], k: usize) -> Self;
-    fn query(&self, k: usize, idx: usize) -> Self::D;
-    fn set_next(&mut self, x: Self::D, idx: usize);
-}
+
 
 pub struct WeightDoubling<T> {
     table: Vec<Vec<Option<(usize, T)>>>,
--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,14 @@
 use cargo_snippet::snippet;
 
+/// 位置`p`から`k`ターン先の行き先を求める
+pub trait Doubling {
+    type D;
+    fn new(v: &[Self::D], k: usize) -> Self;
+    fn query(&self, k: usize, idx: usize) -> Self::D;
+    fn set_next(&mut self, x: Self::D, idx: usize);
+}
+
+
 #[snippet("Monoid")]
 pub trait Monoid {
     /// モノイドの二項演算
@@ -7,4 +16,3 @@ pub trait Monoid {
     /// 二項演算の単位元
     fn e() -> Self;
 }
-
